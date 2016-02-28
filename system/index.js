d2a8e63af0a49027af89687d0777b8b529811ed3
贾顺名 <jiashunming@outlook.com>
2016-02-28 15:22:26 +0800

    测试node exec提交git

--- system/index.js
+++ system/index.js
@@ -1,26 +1,109 @@
+"use strict";
 var express = require("express"),
-	app = express(),
-    bodyParser = require("body-parser");
+    app = express(),
+    fs = require("fs"),
+    uuid = require("node-uuid"),
+    bodyParser = require("body-parser"),
+    feedPath = "../feed/", // 博客数据存储位置
+    feddIndex = "index.js"; // 博客目录文件名
 
 app.set("view engine", "ejs");
-app.use(express.static(`${__dirname}/resource`)); //, "/add"
+app.use(express.static(`${__dirname}/resource`)); //
 app.set("views", `${__dirname}/views`);
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.get(["/", "/index"], (req, res) => {
-	res.render("index");
+    res.render("index");
 });
 app.get("/newBlog", (req, res) => {
-	res.render("newBlog", {
-		userId: 233
-	})
+    res.render("newBlog")
 });
 app.post("/newBlog", (req, res) => {
-	var content = req.body.blogContent;
-	res.render("test_show", {
-		content: content,
-		test: content.replace(/\n/g, "")
-	})
-})
-app.listen(12306);
+    var end = "]});";
+    fs.readFile(`${feedPath}${feddIndex}`, (err, data) => {
+        var indexContent = data.toString(),
+            id = uuid.v1(); // 唯一标识
+        while (new RegExp(id).test(indexContent)) { // 防止重名
+            console.log("一般来说是不会进来的");
+            id = uuid.v1();
+        }
+        var now = new Date(),
+            postDate = `${now.getFullYear()}/${now.getMonth() + 1}/${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`, // 发表时间
+            title = req.body.blogTitle, // 博文标题
+            content = req.body.blogContent.replace(/\n|\r/g, ""); // 博文内容
+
+        console.log("博文js文件开始生成：")
+        fs.writeFile(`../feed/${id}.js`, `define({"id":"${id}","title":"${title}","postDate":"${postDate}","content":"${content.replace(/(\"|\')+?/g, "\\$1")}"});`, (err) => {
+            if (err) {
+                console.error("博文js文件生成出错了。");
+            } else {
+                console.log("博文js文件生成完毕。");
+                buildIndex(); // 重新生成index.js
+                res.writeHeader(200, { 'Content-Type': 'text/html;charset=UTF-8' })
+                res.end("<h1>博客提交完成</h1>");
+            }
+        })
+    });
+});
+app.listen(12306, () => {
+    console.log("runing at 12306");
+});
+
+// 调用该函数 重新生成目录.js
+function buildIndex() { // 生成博文目录的数据
+    let items = []; // 博文列表
+    var files = fs.readdirSync(feedPath);
+    // 获取所有的博文
+    files.forEach((item) => {
+        if (item != feddIndex) { // 跳过目录.js
+            let itemPath = `${feedPath}${item}`;
+            let stats = fs.statSync(itemPath);
+            if (!stats.isDirectory()) { // 如果是文件
+                let data = fs.readFileSync(itemPath);
+                // 将适用于require的js文件砍掉一些，以转换为标准json格式
+                var str = data.toString().replace(/^define\(|\,\"?content\"?\:.*\)\;$/g, "") + "}";
+                if (/\{id\:/.test(str)) { // 如果json的key不标准（没有双引号）给他加上
+                    str = str.replace(/(\w+)?\:\"/g, "\"$1\"\:\"");
+                }
+                var obj = JSON.parse(str);
+                //console.log(obj.title);
+                items.push(obj);
+            }
+        }
+    });
+
+    items = items.sort((left, right) => {
+        return new Date(left.postDate) - new Date(right.postDate)
+    })
+    var indexRender = `define(${JSON.stringify({data : items})});`;
+
+    console.log("开始生成目录文件：");
+    fs.writeFile(`${feedPath}${feddIndex}`, indexRender, (err) => {
+        if (err) {
+            console.error("目录文件生成失败。");
+        } else {
+            console.log("目录文件生成完毕。")
+        }
+    });
+}
+
+function gitCommit() {
+
+}
+var exec = require('child_process').exec,
+    cmd = 'git add index.js';
+exec(cmd, (error) => {
+    if (error) {
+        console.error(error);
+    } else {
+        let cmd = 'git commit -m "测试node exec提交git"'
+        exec(cmd, (error) => {
+            if (error) {
+                console.error(error);
+            } else {
+                console.log("提交完成");
+            }
+        })
+    }
+});
