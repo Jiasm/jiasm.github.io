298ed114351f18097f2f75c9289f20c3c5a2d394
nieweidong <darrencode@qq.com>
2016-04-23 13:44:52 +0800

    Update

--- tools/findcity.js
+++ tools/findcity.js
@@ -5,7 +5,7 @@ let area = require('../data/area_zh.js');
 module.exports = function(num) {
   let res = '未知';
   if (area[num]) {
-    res = area[num];
+    res = area[num].replace('_', '-');
   }
   return res;
 };
