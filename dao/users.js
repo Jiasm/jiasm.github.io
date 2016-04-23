72d8d69af31ec09819e91ec350d66b783389a02b
nieweidong <darrencode@qq.com>
2016-04-23 09:43:10 +0800

    Update

--- dao/users.js
+++ dao/users.js
@@ -7,28 +7,43 @@ let findcity = require('../tools/findcity.js');
 let q = require('../tools/query.js');
 
 function* getDailyActive(query) {
+  let res = [];
+  let obj = {};
+  let date = moment(query.date).format('YYYYMMDD');
+  let stats = query.stats === 'date' ? 'date' : 'date,' + query.stats;
+  let groupbyStr = (stats === 'date') ? 'GROUP BY date' : 'GROUP BY date,' + stats;
+  let orderbyStr = (stats === 'date') ? 'ORDER BY date ASC' : 'ORDER BY active DESC';
+
+  let sql = util.format('SELECT %s FROM %s %s %s %s',
+    stats + ', SUM(active) AS active',
+    'users_count',
+    'WHERE city_settled!=\'1_999_000000\' AND date>=' + date + ' AND date<=' + moment(date).add(6, 'days').format('YYYYMMDD'),
+    groupbyStr,
+    orderbyStr);
+
+  let list = yield q(ak47, sql);
+  for (let i = 0; i < list.length; i++) {
+    if (obj[i]) {
+
+    }
+  }
+
+  return list
+}
+
+function* getDailyReg(query) {
   let date = moment(query.date).format('YYYYMMDD');
   let stats = query.stats || 'date';
+  let orderbyStr = (stats === 'date') ? 'ORDER BY date ASC' : 'ORDER BY reg DESC';
 
-  let params = stats + ' as name,';
-  let groupByStr = 'GROUP BY ' + stats;
-
-  let sql = util.format('SELECT %s FROM %s %s %s %s %s',
-    params + 'SUM(after1) AS a1,SUM(after2) AS a2,SUM(after3) AS a3,SUM(after4) AS a4,SUM(after5) AS a5,SUM(after6) AS a6,SUM(after7) AS a7',
-    'daily_retention',
-    'WHERE date=' + date,
-    groupByStr,
-    'ORDER BY a1 DESC',
-    'LIMIT 0,10');
-
-  let regSql = util.format('SELECT %s FROM %s %s %s %s',
-    params + 'SUM(reg) AS reg',
+  let sql = util.format('SELECT %s FROM %s %s %s %s',
+    stats + ', SUM(reg) AS reg',
     'users_count',
-    'WHERE city_settled!=\'1_999_000000\' AND date=' + date,
-    groupByStr,
-    'ORDER BY reg DESC');
-  return {}
-// return yield handleResult(sql, regSql, stats);
+    'WHERE city_settled!=\'1_999_000000\' AND date>=' + date + ' AND date<=' + moment(date).add(6, 'days').format('YYYYMMDD'),
+    'GROUP BY ' + stats,
+    orderbyStr);
+
+  return yield q(ak47, sql);
 }
 
 function* handleResult(sql, regSql, stats) {
@@ -48,6 +63,5 @@ function* handleResult(sql, regSql, stats) {
 
 module.exports = {
   getDailyActive: getDailyActive,
-// weekly: getWeekly,
-// monthly: getMonthly,
+  getDailyReg: getDailyReg,
 }
